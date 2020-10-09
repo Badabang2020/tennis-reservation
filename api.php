0580fbb77dda25cb495403074c15e288d803ef22
Ben Gombl <gomboc95@gmx.at>
2020-10-09 15:03:19 +0200

    created add(), edit(), delete(), get() functions and schwitzkäs. add()  creates an sql INSERT statement and executes it.

--- api.php
+++ api.php
@@ -6,6 +6,7 @@ $password = "oa6O8g,6movs";
 $dbname = "usr_p452177_4";
 
 //CREATE CONNECTION
+
 $connection = new mysqli($servername, $username, $password, $dbname);
 
 //CHECK CONNECTION
@@ -27,6 +28,72 @@ if ($connection->connect_error)
 // } else
 //     echo ("Zero results.");
 
-$connection->close();
+//$connection->close();
+?>
+
+<!-- Functions!-->
+
+<?php
+    //$_REQUEST ['purpose': 'myPurpose', 'target': 'myTarget', 'values': myValues['key1': 'value1', 'key2': 'value2', 'key3': 'value3',...] ]
+    if ($_REQUEST) {
+
+        //get purpose
+        $purpose = $_REQUEST['purpose'];
+        
+        switch($_REQUEST['target']) {
+            
+        }
+        switch($purpose) {
+            case "add": add();
+            break;
+            case "edit": edit();
+            break;
+            case "delete": delete();
+            break;
+            case "get": get();
+            break;
+        }
+
+        //add to database
+        //creates an sql INSERT statement based on values of $_REQUEST, and executes it.
+        function add(){
+            
+            $target = $_REQUEST['target'];
+            // <-------HEAD-------><-------KEYSSTRING--------><--MID---><--------VALUESSTRING------------>
+            // INSERT INTO 'target'('key1', 'key2', 'key3',...) VALUES ('value1', 'value2', 'value3',...);
+            $queryHead = "INSERT INTO '".$target."'(";
+            $keysString = "";
+            $queryMid = ") VALUES (";
+            $valuesString = "";
+            foreach ($_REQUEST['fields'] as $key => $value) {
+                $keysString .= ",'".$key."'";
+                $valuesString .= ",'".$value."'";
+            }
+            $valuesString.=");";
+            //first char of $keysString and $valuesString is an unwanted ','
+            $query = $queryHead.substr($keysString, 1).$queryMid.substr($valuesString, 1);
+            global $connection;
+            $connection->query($query);
+            return;
+        }
+
+
+        //delete from database
+        function delete(){
+
+        }
+
+
+        //edit data in database
+        function edit(){
+
+        }
+
+
+        //get data from database
+        function get(){
+
+        }
+    }
 
 ?>
