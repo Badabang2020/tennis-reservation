07b4094d514d4798c7657691cda8bd61f162a00d
Matthias Neuwersch <office@neuwersch.eu>
2020-10-09 10:03:40 +0200

    setup main and bash

--- js/bash/bash-pwa.js
+++ js/bash/bash-pwa.js
@@ -1,8 +1,16 @@
 "use strict";
-
+//TODO: IMPORT model and api
 /********************************
  *   Class-Bundle for PWAs.
  *
  *
  *
  ********************************/
+export default class bash_pwa{
+    constructor(){
+        //TODO Alle möglichen Properties.
+        // this.model = new bash_model();
+        // this.api = new api)(;
+        window.bash = this;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 //Gemeinsam: IMPORT
+"use strict";
+import Bash_PWA from './bash/bash-pwa';
+
+const Bash_PWA = new Bash_PWA();
