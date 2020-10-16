bc064f0aa789706fcdc56184f65e387761cfbefa
Raphael <raphael.m.resl@gmail.com>
2020-10-16 11:51:14 +0200

    raphael new view & tpl + altered main

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@
 import Bash_PWA from './bash/bash-pwa.js';
 import LoginView from "./views/view.login.js";
 import MainMenuView from "./views/view.mainmenu.js";
+import RaphaelView from './views/view.raphael.js';
 
 //Routen-Array anlegen
 let routes = [
     new MainMenuView("/", "mainmenu"),
-    new LoginView("/login", "login")
+    new LoginView("/login", "login"),
+    new RaphaelView("/raphael", "raphael")
 ];
 
-const Bash = new Bash_PWA("http://127.0.0.1/tennis_reservation/", "templates", routes);
+const Bash = new Bash_PWA("http://127.0.0.1/tennis-reservation/", "templates", routes);
 // const Bash = new Bash_PWA("http://p452177.mittwaldserver.info/platzreservierung/", "templates", routes);
