de28de6b2b2fafb9f38c0a757c4be1af7dcb062a
Ben Gombl <gomboc95@gmx.at>
2020-10-16 12:38:50 +0200

    commite alles

--- js/main.js
+++ js/main.js
@@ -4,12 +4,24 @@ import Bash_PWA from './bash/bash-pwa.js';
 import LoginView from "./views/view.login.js";
 import MainMenuView from "./views/view.mainmenu.js";
 import BenView from "./views/view.ben.js";
+import FabianView from './views/view.fabian.js';
+import MatthiasView from './views/view.matthias.js';
+import MichaelBView from './views/view.michaelB.js';
+import michaelW from './views/view.michaelW.js';
+import RaphaelView from './views/view.raphael.js';
+import SebastianView from './views/view.sebastian.js';
 
 //Routen-Array anlegen
 let routes = [
     new MainMenuView("/", "mainmenu"),
     new LoginView("/login", "login"),
-    new BenView("/ben", "ben")
+    new BenView("/ben", "ben"),
+    new FabianView("/fabian", "fabian"),
+    new MatthiasView("/matthias", "matthias"),
+    new MichaelBView("/michaelb", "michaelb"),
+    new michaelW("/michaelw", "michaelw"),
+    new RaphaelView("/raphael", "raphael"),
+    new SebastianView("/sebastian", "sebastian")
 ];
 
 const Bash = new Bash_PWA("http://127.0.0.1/tennis/", "templates", routes);
--- js/views/view.sebastian.js
+++ js/views/view.sebastian.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 import Bash_Route from "./../bash/bash-spa-route.js";
 
@@ -14,21 +13,4 @@ export default class SebastianView extends Bash_Route{
            window.location.hash = "/";
         });
     }
-=======
-"use strict";
-import Bash_Route from "./../bash/bash-spa-route.js";
-
-export default class SebastianView extends Bash_Route{
-    constructor(slug, template) {
-        super(slug, template);
-    }
-
-    //OVERWRITE
-    init(){
-        $("#hello-from-sebastian").unbind("click").on("click", function(){
-          
-           window.location.hash = "/";
-        });
-    }
->>>>>>> develop
 }
