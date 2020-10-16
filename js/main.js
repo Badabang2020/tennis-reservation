bd4be45c7d92378cbb2c0935cb35f47203c6611d
Ben Gombl <gomboc95@gmx.at>
2020-10-16 11:58:19 +0200

    benview

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@
 import Bash_PWA from './bash/bash-pwa.js';
 import LoginView from "./views/view.login.js";
 import MainMenuView from "./views/view.mainmenu.js";
+import BenView from "./views/view.ben.js";
 
 //Routen-Array anlegen
 let routes = [
     new MainMenuView("/", "mainmenu"),
-    new LoginView("/login", "login")
+    new LoginView("/login", "login"),
+    new BenView("/ben", "ben")
 ];
 
-const Bash = new Bash_PWA("http://127.0.0.1/tennis_reservation/", "templates", routes);
+const Bash = new Bash_PWA("http://127.0.0.1/tennis/", "templates", routes);
 // const Bash = new Bash_PWA("http://p452177.mittwaldserver.info/platzreservierung/", "templates", routes);
---
+++ templates/ben.tpl
@@ -0,0 +1,5 @@
+ <div class="Ben">
+    <h1>Ben</h1>
+    <li data-slug = "fabian">Fabian</li>
+    <li data-slug = "">Main</li>
+</div>
