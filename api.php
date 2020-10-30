6613dcad5d06825ef1a523aebdf7671827271fae
Ioan-Sebastian Szücs <ioan.szucs@gmail.com>
2020-10-30 10:35:04 +0100

    addCourt  added.

--- api.php
+++ api.php
@@ -4,59 +4,85 @@ $username = "p452177d1";
 $password = "oa6O8g,6movs";
 $dbname = "usr_p452177_4";
 
-$connection = new mysqli($servername, $username, $password, $dbname);
+$_GLOBAL['connection'] = new mysqli($servername, $username, $password, $dbname);
 
-if ($connection->connect_error)
-    die("Connection failed: " . $connection->connect_error);
+$_GLOBAL['data'] = json_decode($_REQUEST["data"]);
+
+
+if ($_GLOBAL['connection']->connect_error)
+    die("Connection failed: " . $_GLOBAL['connection']->connect_error);
 
 if(isset($_REQUEST["purpose"])){
     switch($_REQUEST["purpose"]){
         case "Login":
-            login($connection);
+            login();
             break;
         case "RegisterClub":
-            registerClub($connection);
+            registerClub();
             break;
         case "RegisterMember":
-            registerMember($connection);
+            registerMember();
             break;
         case "AddCourt":
-            addCourt($connection);
+            addCourt();
             break;
         case "DeleteCourt":
-            deleteCourt($connection);
+            deleteCourt();
             break;
         default:
             break;
     }
 }
 
-function login($connection){
-    $data = json_decode($_REQUEST["data"]);
-    $query = "SELECT * FROM member WHERE email= '".$data->email."' AND password = '".sha1($data->password)."'";
-    $user = $connection->query($query);
+function login(){
+    $query = "SELECT * FROM member WHERE email= '".$_GLOBAL['data']->email."' AND password = '".sha1($_GLOBAL['data']->password)."'";
+    $user = $_GLOBAL['connection']->query($query);
     if($user->num_rows>0){
         die(json_encode($user->fetch_assoc()));
     } else
         die(false);
 }
 
-function addCourt($connection){
-    $data = json_decode($_REQUEST["data"]);
+function addCourt(){
+    $query = "INSERT INTO court(clubname, name, surface) VALUES (".$_GLOBAL['data']->clubname.",".$_GLOBAL['data']->name.",".$_GLOBAL['data']->surface.");";
+
+    $result = $_GLOBAL['connection']->query($query);
+    if($result) 
+        die(json_encode($_GLOBAL['connection']->insert_id));
+    else
+        die("Could not add court.");
+
 
 }
 
-function registerMember($connection){
-    $data = json_decode($_REQUEST["data"]);
+function registerMember(){
     $query = "INSERT INTO member (first_name, last_name, gender, birthday, phonenumber, email, password, role, clubname) 
-    VALUES ('".$data->first_name.",".$data->last_name.",".$data->gender.",".$data->birthday.",".$data->phonenumber.",".$data->email.",".sha1($data->password).",".$data->role.",".$data->clubname.");";
-    $result = $connection->query($query);
-    if($result) {
-        die(json_encode($connection->insert_id));
-    } else {
+    VALUES (".$_GLOBAL['data']->first_name.",".$_GLOBAL['data']->last_name.",".$_GLOBAL['data']->gender.",".$_GLOBAL['data']->birthday.",".$_GLOBAL['data']->phonenumber.",".$_GLOBAL['data']->email.",".sha1($_GLOBAL['data']->password).",".$_GLOBAL['data']->role.",".$_GLOBAL['data']->clubname.");";
+    $result = $_GLOBAL['connection']->query($query);
+    if($result) 
+        die(json_encode($_GLOBAL['connection']->insert_id));
+    else
         die("Could not register member.");
-    }
+}
+
+function registerClub(){
+    $query = "INSERT INTO club (clubname, address, postcode) 
+    VALUES (".$_GLOBAL['data']->clubname.",".$_GLOBAL['data']->address.",".$_GLOBAL['data']->postcode.");";
+    $result = $_GLOBAL['connection']->query($query);
+    if($result) 
+        die(json_encode($_GLOBAL['connection']->insert_id));
+    else
+        die("Could not register club.");
 }
 
 
-$connection->close();
+function deleteCourt(){
+
+    $query= "DELETE FROM court WHERE courtid=".$_GLOBAL['data']->id.";";
+    $result= $_GLOBAL['connection']->query($query);
+    die($result); 
+}
+
+
+
+$_GLOBAL['connection']->close();
