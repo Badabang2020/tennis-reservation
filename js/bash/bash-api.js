bc732df6a3c67b4c8fda35176d91e9bf00ee356f
Noran <matschiff@gmx.de>
2020-10-30 12:21:08 +0100

    Added edit, get and remove functions

--- js/bash/bash-api.js
+++ js/bash/bash-api.js
@@ -15,6 +15,7 @@ export default class API {
         API.request("Login", JSON.stringify(credentials), callback);
     }
 
+    /////////////////////// Club Functions ///////////////////////////////
     registerClub(clubname, adress, postcode, logo, hoursAtOnce, hoursPerWeek, openFrom, openUntil, daysBefore, email, first_name, last_name, gender, birthday, phonenumber, password, callback) {
         let clubInfo = {
             clubname: clubname,
@@ -34,6 +35,28 @@ export default class API {
         });
     }
 
+    editClub(clubname, adress, postcode, logo, hoursAtOnce, hoursPerWeek, openFrom, openUntil, daysBefore, callback) {
+        let clubInfo = {
+            clubname: clubname,
+            adress: adress,
+            postcode: postcode,
+            logo: logo,
+            hoursAtOnce: hoursAtOnce,
+            hoursPerWeek: hoursPerWeek,
+            openFrom: openFrom,
+            openUntil: openUntil,
+            daysBefore: daysBefore
+        };
+        API.request("EditClub", JSON.stringify(clubInfo), callback)
+    }
+
+    getClub(clubname, callback) {
+        let club = {
+            clubname: clubname
+        };
+        API.request("GetClub", JSON.stringify(club), callback);
+    }
+    /////////////////////// Member Functions ///////////////////////////////
     registerMember(email, first_name, last_name, gender, birthday, phonenumber, password, role, clubname, callback) {
         let memberInfo = {
             email: email,
@@ -44,12 +67,48 @@ export default class API {
             phonenumber: phonenumber,
             password: password,
             role: role,
-            clubname: clubname,
+            clubname: clubname
         };
         API.request("RegisterMember", JSON.stringify(memberInfo), callback);
     }
 
-    addCourt(clubname, name, surface) {
+    deleteMember(membernumber, callback) {
+        let memberId = {
+            membernumber: membernumber
+        };
+        API.request("DeleteMember", JSON.stringify(memberId), callback);
+    }
+
+    editMember(email, first_name, last_name, gender, phonenumber, password, birthday, role, callback) {
+        let memberInfo = {
+            email: email,
+            first_name: first_name,
+            last_name: last_name,
+            gender: gender,
+            birthday: birthday,
+            phonenumber: phonenumber,
+            password: password,
+            role: role,
+        }
+        API.request("EditMember", JSON(memberInfo), callback)
+    }
+
+    getMembersOfClub(clubname, callback) {
+        let clubInfo = {
+            clubname: clubname
+        };
+        API.request("GetMembersOfClub", JSON.stringify(clubInfo), callback);
+    }
+
+    getMember(membernumber, callback) {
+        let memberInfo = {
+            membernumber: membernumber
+        };
+        API.request("GetMembersOfClub", JSON.stringify(memberInfo), callback);
+    }
+
+    /////////////////////// Court Functions ///////////////////////////////
+    addCourt(clubname, name, surface, callback) {
         let courtInfo = {
             clubname: clubname,
             name: name,
@@ -65,6 +124,30 @@ export default class API {
         API.request("DeleteCourt", JSON.stringify(courtInfo), callback);
     }
 
+    editCourt(clubname, name, surface, callback) {
+        let courtInfo = {
+            clubname: clubname,
+            name: name,
+            surface: surface
+        };
+        API.request("Court", JSON.stringify(courtInfo), callback);
+    }
+
+    getCourtsOfClub(clubname, callback) {
+        let clubInfo = {
+            clubname: clubname
+        };
+        API.request("getCourtsOfClub", JSON.stringify(clubInfo), callback);
+    }
+
+    getCourt(courtid, callback) {
+        let courtInfo = {
+            courtid: courtid
+        };
+        API.request("getCourt", JSON.stringify(courtInfo), callback);
+    }
+
+    /////////////////////// API request ///////////////////////////////
     static request(purpose, json, callback) {
         $.ajax({
             url: "http://p452177.mittwaldserver.info/platzreservierung/api.php",
