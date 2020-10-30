f209ab004ecf9103510b3c75a758fb2183f952da
Noran <matschiff@gmx.de>
2020-10-30 11:51:04 +0100

    add functions in bash-api: - register Club - register Member - add Court - remove Court

--- js/bash/bash-api.js
+++ js/bash/bash-api.js
@@ -4,26 +4,76 @@
  *  Transmition via JSON :)
  *  Wish you a great day!
  *********************************************/
-export default class API{
-    constructor() {}
+export default class API {
+    constructor() { }
 
-    login(email, password, callback){
+    login(email, password, callback) {
         let credentials = {
             email: email,
-            password : password
+            password: password
         };
         API.request("Login", JSON.stringify(credentials), callback);
     }
 
-    static request(purpose, json, callback){
+    registerClub(clubname, adress, postcode, logo, hoursAtOnce, hoursPerWeek, openFrom, openUntil, daysBefore, email, first_name, last_name, gender, birthday, phonenumber, password, callback) {
+        let clubInfo = {
+            clubname: clubname,
+            adress: adress,
+            postcode: postcode,
+            logo: logo,
+            hoursAtOnce: hoursAtOnce,
+            hoursPerWeek: hoursPerWeek,
+            openFrom: openFrom,
+            openUntil: openUntil,
+            daysBefore: daysBefore
+        };
+        let self = this;
+        API.request("RegisterClub", JSON.stringify(clubInfo), function (data) {
+            if (data != "Could not register club")
+                self.registerMember(email, first_name, last_name, gender, birthday, phonenumber, password, "admin", clubname, callback);
+        });
+    }
+
+    registerMember(email, first_name, last_name, gender, birthday, phonenumber, password, role, clubname, callback) {
+        let memberInfo = {
+            email: email,
+            first_name: first_name,
+            last_name: last_name,
+            gender: gender,
+            birthday: birthday,
+            phonenumber: phonenumber,
+            password: password,
+            role: role,
+            clubname: clubname,
+        };
+        API.request("RegisterMember", JSON.stringify(memberInfo), callback);
+    }
+
+    addCourt(clubname, name, surface) {
+        let courtInfo = {
+            clubname: clubname,
+            name: name,
+            surface: surface
+        };
+        API.request("AddCourt", JSON.stringify(courtInfo), callback);
+    }
+
+    removeCourt(courtid, callback) {
+        let courtInfo = {
+            courtid: courtid
+        };
+        API.request("DeleteCourt", JSON.stringify(courtInfo), callback);
+    }
+
+    static request(purpose, json, callback) {
         $.ajax({
             url: "http://p452177.mittwaldserver.info/platzreservierung/api.php",
             data: {
-                purpose : purpose,
-                data : json
+                purpose: purpose,
+                data: json
             },
             method: 'POST'
-        }).done(function(data){
+        }).done(function (data) {
             callback(data);
         });
     }
