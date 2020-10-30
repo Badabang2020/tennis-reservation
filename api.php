00f9f21dee7fcb166c2c0f255420df022960321e
Matthias Neuwersch <office@neuwersch.eu>
2020-10-30 10:40:55 +0100

    lil refactoring

--- api.php
+++ api.php
@@ -4,13 +4,13 @@ $username = "p452177d1";
 $password = "oa6O8g,6movs";
 $dbname = "usr_p452177_4";
 
-$_GLOBAL['connection'] = new mysqli($servername, $username, $password, $dbname);
+$GLOBALS['connection'] = new mysqli($servername, $username, $password, $dbname);
 
-$_GLOBAL['data'] = json_decode($_REQUEST["data"]);
+$GLOBALS['data'] = json_decode($_REQUEST["data"]);
 
 
-if ($_GLOBAL['connection']->connect_error)
-    die("Connection failed: " . $_GLOBAL['connection']->connect_error);
+if ($GLOBALS['connection']->connect_error)
+    die("Connection failed: " . $GLOBALS['connection']->connect_error);
 
 if(isset($_REQUEST["purpose"])){
     switch($_REQUEST["purpose"]){
@@ -35,8 +35,8 @@ if(isset($_REQUEST["purpose"])){
 }
 
 function login(){
-    $query = "SELECT * FROM member WHERE email= '".$_GLOBAL['data']->email."' AND password = '".sha1($_GLOBAL['data']->password)."'";
-    $user = $_GLOBAL['connection']->query($query);
+    $query = "SELECT * FROM member WHERE email= '".$GLOBALS['data']->email."' AND password = '".sha1($GLOBALS['data']->password)."'";
+    $user = $GLOBALS['connection']->query($query);
     if($user->num_rows>0){
         die(json_encode($user->fetch_assoc()));
     } else
@@ -44,45 +44,29 @@ function login(){
 }
 
 function addCourt(){
-    $query = "INSERT INTO court(clubname, name, surface) VALUES (".$_GLOBAL['data']->clubname.",".$_GLOBAL['data']->name.",".$_GLOBAL['data']->surface.");";
-
-    $result = $_GLOBAL['connection']->query($query);
-    if($result) 
-        die(json_encode($_GLOBAL['connection']->insert_id));
-    else
-        die("Could not add court.");
-
-
+    $query = "INSERT INTO court(clubname, name, surface) VALUES ('".$GLOBALS['data']->clubname."','".$GLOBALS['data']->name."','".$GLOBALS['data']->surface."');";
+    $result = $GLOBALS['connection']->query($query);
+    die($result ? json_encode($GLOBALS['connection']->insert_id) : "Could not add court");
 }
 
 function registerMember(){
     $query = "INSERT INTO member (first_name, last_name, gender, birthday, phonenumber, email, password, role, clubname) 
-    VALUES (".$_GLOBAL['data']->first_name.",".$_GLOBAL['data']->last_name.",".$_GLOBAL['data']->gender.",".$_GLOBAL['data']->birthday.",".$_GLOBAL['data']->phonenumber.",".$_GLOBAL['data']->email.",".sha1($_GLOBAL['data']->password).",".$_GLOBAL['data']->role.",".$_GLOBAL['data']->clubname.");";
-    $result = $_GLOBAL['connection']->query($query);
-    if($result) 
-        die(json_encode($_GLOBAL['connection']->insert_id));
-    else
-        die("Could not register member.");
+    VALUES ('".$GLOBALS['data']->first_name."',''".$GLOBALS['data']->last_name."','".$GLOBALS['data']->gender."','".$GLOBALS['data']->birthday."','".$GLOBALS['data']->phonenumber."','".$GLOBALS['data']->email."','".sha1($GLOBALS['data']->password)."',".$GLOBALS['data']->role.",'".$GLOBALS['data']->clubname."');";
+    $result = $GLOBALS['connection']->query($query);
+    die($result ? json_encode($GLOBALS['connection']->insert_id) : "Could not register member");
 }
 
 function registerClub(){
     $query = "INSERT INTO club (clubname, address, postcode) 
-    VALUES (".$_GLOBAL['data']->clubname.",".$_GLOBAL['data']->address.",".$_GLOBAL['data']->postcode.");";
-    $result = $_GLOBAL['connection']->query($query);
-    if($result) 
-        die(json_encode($_GLOBAL['connection']->insert_id));
-    else
-        die("Could not register club.");
+    VALUES ('".$GLOBALS['data']->clubname."','".$GLOBALS['data']->address."','".$GLOBALS['data']->postcode."');";
+    $result = $GLOBALS['connection']->query($query);
+    die($result ? json_encode($GLOBALS['connection']->insert_id) : "Could not register club");
 }
 
-
 function deleteCourt(){
-
-    $query= "DELETE FROM court WHERE courtid=".$_GLOBAL['data']->id.";";
-    $result= $_GLOBAL['connection']->query($query);
+    $query= "DELETE FROM court WHERE courtid=".$GLOBALS['data']->id.";";
+    $result= $GLOBALS['connection']->query($query);
     die($result); 
 }
 
-
-
-$_GLOBAL['connection']->close();
+$GLOBALS['connection']->close();
