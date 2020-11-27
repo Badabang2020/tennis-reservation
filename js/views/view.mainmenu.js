4c198e96668506b1112a60256622111873f32bd4
Bruckii <michaelbruckbauer@gmx.at>
2020-11-27 10:37:49 +0100

    added table, changed some css

--- js/views/view.mainmenu.js
+++ js/views/view.mainmenu.js
@@ -13,7 +13,11 @@ export default class MainMenuView extends Bash_Route{
         if(!window.bash.utils.getCookie("user")) {
             window.location.hash = "/login";
         } else {
-            this.role = JSON.parse(window.bash.utils.getCookie("user")).role;
+            try {
+                this.role = JSON.parse(window.bash.utils.getCookie("user")).role;
+            } catch (error) {
+                console.error(error);                
+            }
             this.renderView();
             $("#logout").unbind("click").on("click", function(){
                window.bash.utils.deleteCookie("user");
@@ -32,22 +36,35 @@ export default class MainMenuView extends Bash_Route{
         // });
         //let daysBefore = window.bash.model.club.daysBefore;
         let daysBefore = 14;
+        var date = new Date();
         for (let i = 0; i < daysBefore; i++){
-            this.renderTable();
+            this.renderTable(date);
+            date.setDate(date.getDate() + 1);
         }
     }
-
-    renderTable() {
+    
+    renderTable(date) {
+        date = date.toLocaleDateString();
+        let courtArr = ["Platz 1", "Platz 2", "Platz 3"];
         let table = "<table><tr>";
         let startHour = 8.0;
         let startMin = 0;
         let endHour = 22.0;
         let endMin = 0;
-        let minute = 0
-        for (let hour = startHour; hour <= endHour; hour++){
+        let minute = 0;
+        // Creating tableheader (times)
+        table += "<th>" + date + "</th>";
+        for (let hour = startHour; hour <= endHour; hour++)
             table += "<th>"+hour+"</th>";
-        }
-
-        $(".maincontent").html(table);
+        // Creating tabledata
+        table += "</tr>";
+        courtArr.forEach(court => {
+            table += "<tr><td>" + court + "</td>";
+            for (let hour = startHour; hour <= endHour; hour++)
+                table += "<td>"+ "COVID" + "</td>";
+        });
+        table += "</tr></table>";
+        
+        $(".maincontent").append(table);
     }
 }
