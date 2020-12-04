444b0eff37a12df6330b7f0ff7d4778d25f935aa
iss1977 <ioan.szucs@gmail.com>
2020-12-04 10:57:52 +0100

    clubsettings branch made.

--- js/views/view.clubsettings.js
+++ js/views/view.clubsettings.js
@@ -27,24 +27,39 @@ export default class ClubSettingsView extends Bash_Route {
             // console.log(jsonMembers);
             for(const member of jsonMembers){
                 console.log(member);
-                
-                $("#clubadmin__deletemember").unbind("click").on("click", function(){
-                        console.log("ACTION: Delete member");
-                    });
 
+                
+                
                     
 
 
-                $("#club-settings-members-table-body").append("<tr>"+
+                $("#club-settings-members-table-body").append("<tr data-member-number='"+member.membernumber+"'>"+
                 "<td>"+member.first_name+" "+member.last_name+"</td>"+
                 "<td>"+member.email+"</td>"+
                 "<td>"+member.adress+"</td>"+
                 "<td>"+
-                "  <button id=\"clubadmin__editmember\"><%>cEdit<%></button>"+
-                "  <button id=\"clubadmin__deletemember\"><%>cDelete<%></button>"+
+                "  <button class='clubadmin__editmember'>"+window.bash.t("cEdit")+"</button>"+
+                "  <button class='clubadmin__deletemember'>"+window.bash.t("cDelete")+"</button>"+
                 "</td>"+
                 "</tr>");
                 //Mit diesen Members foreach Memberzeile aufbauen.
+
+                $(".clubadmin__deletemember").unbind("click").on("click", function(e){
+                    e.preventDefault();
+                    console.log($(this).closest("tr").data("member-number"));
+                    console.log($(this).parent().parent());
+                    console.log("ACTION: Delete member");
+                });
+
+                $(".clubadmin__editmember").unbind("click").on("click", function(e){
+                    e.preventDefault();
+                    console.log($(this).closest("tr").data("member-number"));
+                    console.log($(this).parent().parent());
+                    console.log("ACTION: Edit member");
+                    
+                });
+
+
             }
         })
     }
